beb7fa78029a72a001afe1888d97123b60cad932
Katherine Moran <on3hwmjm29@osxofulk.com>
2025-07-07 19:10:11 -0500

    commit adicionales

--- src/test/java/com/minimercado/sgm/controller/ProveedorControllerTest.java
+++ src/test/java/com/minimercado/sgm/controller/ProveedorControllerTest.java
@@ -3,9 +3,7 @@ package com.minimercado.sgm.controller;
 import com.minimercado.sgm.model.Proveedor;
 import com.minimercado.sgm.service.ProveedorService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -22,7 +20,7 @@ class ProveedorControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    
     private ProveedorService proveedorService;
 
     @Test
